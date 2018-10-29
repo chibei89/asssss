65f6faae08813c8f392ed98041df1b44e3dfe33b
chibei89 <chibei89@163.com>
2018-10-29 21:51:18 +0800

    提交项目1

---
+++ untitled/src/cn/chibei/Demo22.java
@@ -0,0 +1,11 @@
+package cn.chibei;
+
+public class Demo22 {
+    public static void main(String[] args) {
+        System.out.println("春有百花秋有月,夏有凉风冬有雪");
+        System.out.println("春有百花秋有月,夏有凉风冬有雪");
+        System.out.println("春有百花秋有月,夏有凉风冬有雪");
+        System.out.println("春有百花秋有月,夏有凉风冬有雪");
+        System.out.println("春有百花秋有月,夏有凉风冬有雪");
+    }
+}
